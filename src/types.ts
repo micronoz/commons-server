599b54fc77792ac85ce36729f1e7e9e512e9eb47
Nabi Ozberkman <n.ozberkman@gmail.com>
2021-02-19 22:15:26 -0800

    It

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,7 @@
-import { EntityManager } from 'typeorm';
 import { Response, Request } from 'express';
 import { User } from './entity/User';
 
 export type MyContext = {
-  em: EntityManager;
   req: Request;
   res: Response;
   user: User;
