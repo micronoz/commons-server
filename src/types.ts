8d13b83ded6b97ef8c5191f3c8af8a162ee82813
Nabi Ozberkman <n.ozberkman@gmail.com>
2021-03-09 22:38:24 -0800

    Switch back to type

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,9 @@ import { Response, Request } from 'express';
 import { User } from './entity/User';
 import { FirebaseUser } from './model/firebaseUser';
 
-export class MyContext {
-  public req: Request;
-  public res: Response;
-  public getUser: () => Promise<User>;
-  public firebaseUser: FirebaseUser;
-}
+export type MyContext = {
+  req: Request;
+  res: Response;
+  getUser: () => Promise<User>;
+  firebaseUser: FirebaseUser;
+};
